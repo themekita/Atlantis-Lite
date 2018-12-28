185940cf03cd2c2dd4f30110b603cfaa6e8a6df1
unknown <rianhizrian@gmail.com>
2018-12-28 17:14:42 +0700

    Atlantis Lite

---
+++ examples/assets/js/atlantis.js
@@ -0,0 +1,389 @@
+"use strict";
+
+$(".nav-search .input-group > input").focus(function(e){
+	$(this).parent().addClass("focus");
+}).blur(function(e){
+	$(this).parent().removeClass("focus");
+});
+
+$(function () {
+	$('[data-toggle="tooltip"]').tooltip();
+	$('[data-toggle="popover"]').popover();
+	layoutsColors();
+});
+
+function layoutsColors(){
+	if($('.sidebar').is('[data-background-color]')) { 
+		$('html').addClass('sidebar-color');
+	} else {
+		$('html').removeClass('sidebar-color');
+	}
+
+	if($('body').is('[data-image]')) {
+		$('body').css('background-image', 'url("' + $('body').attr('data-image') + '")');
+	} else {
+		$('body').css('background-image', '');
+	}
+}
+
+function legendClickCallback(event) {
+	event = event || window.event;
+
+	var target = event.target || event.srcElement;
+	while (target.nodeName !== 'LI') {
+		target = target.parentElement;
+	}
+	var parent = target.parentElement;
+	var chartId = parseInt(parent.classList[0].split("-")[0], 10);
+	var chart = Chart.instances[chartId];
+	var index = Array.prototype.slice.call(parent.children).indexOf(target);
+
+	chart.legend.options.onClick.call(chart, event, chart.legend.legendItems[index]);
+	if (chart.isDatasetVisible(index)) {
+		target.classList.remove('hidden');
+	} else {
+		target.classList.add('hidden');
+	}
+}
+
+$(document).ready(function(){
+
+	$('.btn-refresh-card').on('click', function(){var e=$(this).parents(".card");e.length&&(e.addClass("is-loading"),setTimeout(function(){e.removeClass("is-loading")},3e3))})
+
+	var scrollbarDashboard = $('.sidebar .scrollbar');
+	if (scrollbarDashboard.length > 0) {
+		scrollbarDashboard.scrollbar();
+	}
+
+	var contentScrollbar = $('.main-panel .content-scroll');
+	if (contentScrollbar.length > 0) {
+		contentScrollbar.scrollbar();
+	}
+
+	var messagesScrollbar = $('.messages-scroll');
+	if (messagesScrollbar.length > 0) {
+		messagesScrollbar.scrollbar();
+	}
+
+	var tasksScrollbar = $('.tasks-scroll');
+	if (tasksScrollbar.length > 0) {
+		tasksScrollbar.scrollbar();
+	}
+
+	var quickScrollbar = $('.quick-scroll');
+	if (quickScrollbar.length > 0) {
+		quickScrollbar.scrollbar();
+	}
+
+	var messageNotifScrollbar = $('.message-notif-scroll');
+	if (messageNotifScrollbar.length > 0) {
+		messageNotifScrollbar.scrollbar();
+	}
+
+	var notifScrollbar = $('.notif-scroll');
+	if (notifScrollbar.length > 0) {
+		notifScrollbar.scrollbar();
+	}
+
+	var quickActionsScrollbar = $('.quick-actions-scroll');
+	if (quickActionsScrollbar.length > 0) {
+		quickActionsScrollbar.scrollbar();
+	}
+
+	var userScrollbar = $('.dropdown-user-scroll');
+	if (userScrollbar.length > 0) {
+		userScrollbar.scrollbar();
+	}
+
+	$('.scroll-bar').draggable();
+
+	$('#search-nav').on('shown.bs.collapse', function () {
+		$('.nav-search .form-control').focus();
+	});
+
+	var toggle_sidebar = false,
+	toggle_quick_sidebar = false,
+	toggle_topbar = false,
+	minimize_sidebar = false,
+	toggle_page_sidebar = false,
+	toggle_overlay_sidebar = false,
+	nav_open = 0,
+	quick_sidebar_open = 0,
+	topbar_open = 0,
+	mini_sidebar = 0,
+	page_sidebar_open = 0,
+	overlay_sidebar_open = 0;
+
+
+	if(!toggle_sidebar) {
+		var toggle = $('.sidenav-toggler');
+
+		toggle.on('click', function(){
+			if (nav_open == 1){
+				$('html').removeClass('nav_open');
+				toggle.removeClass('toggled');
+				nav_open = 0;
+			}  else {
+				$('html').addClass('nav_open');
+				toggle.addClass('toggled');
+				nav_open = 1;
+			}
+		});
+		toggle_sidebar = true;
+	}
+
+	if(!quick_sidebar_open) {
+		var toggle = $('.quick-sidebar-toggler');
+
+		toggle.on('click', function(){
+			if (nav_open == 1){
+				$('html').removeClass('quick_sidebar_open');
+				$('.quick-sidebar-overlay').remove();
+				toggle.removeClass('toggled');
+				quick_sidebar_open = 0;
+			}  else {
+				$('html').addClass('quick_sidebar_open');
+				toggle.addClass('toggled');
+				$('<div class="quick-sidebar-overlay"></div>').insertAfter('.quick-sidebar');
+				quick_sidebar_open = 1;
+			}
+		});
+
+		$('.wrapper').mouseup(function(e)
+		{
+			var subject = $('.quick-sidebar'); 
+
+			if(e.target.className != subject.attr('class') && !subject.has(e.target).length)
+			{
+				$('html').removeClass('quick_sidebar_open');
+				$('.quick-sidebar-toggler').removeClass('toggled');
+				$('.quick-sidebar-overlay').remove();
+				quick_sidebar_open = 0;
+			}
+		});
+
+		$(".close-quick-sidebar").on('click', function(){
+			$('html').removeClass('quick_sidebar_open');
+			$('.quick-sidebar-toggler').removeClass('toggled');
+			$('.quick-sidebar-overlay').remove();
+			quick_sidebar_open = 0;
+		});
+
+		quick_sidebar_open = true;
+	}
+
+	if(!toggle_topbar) {
+		var topbar = $('.topbar-toggler');
+
+		topbar.on('click', function() {
+			if (topbar_open == 1) {
+				$('html').removeClass('topbar_open');
+				topbar.removeClass('toggled');
+				topbar_open = 0;
+			} else {
+				$('html').addClass('topbar_open');
+				topbar.addClass('toggled');
+				topbar_open = 1;
+			}
+		});
+		toggle_topbar = true;
+	}
+
+	if(!minimize_sidebar){
+		var minibutton = $('.toggle-sidebar');
+		if($('.wrapper').hasClass('sidebar_minimize')){
+			mini_sidebar = 1;
+			minibutton.addClass('toggled');
+			minibutton.html('<i class="icon-options-vertical"></i>');
+		}
+
+		minibutton.on('click', function() {
+			if (mini_sidebar == 1) {
+				$('.wrapper').removeClass('sidebar_minimize');
+				minibutton.removeClass('toggled');
+				minibutton.html('<i class="icon-menu"></i>');
+				mini_sidebar = 0;
+			} else {
+				$('.wrapper').addClass('sidebar_minimize');
+				minibutton.addClass('toggled');
+				minibutton.html('<i class="icon-options-vertical"></i>');
+				mini_sidebar = 1;
+			}
+			$(window).resize();
+		});
+		minimize_sidebar = true;
+	}
+
+	if(!toggle_page_sidebar) {
+		var pageSidebarToggler = $('.page-sidebar-toggler');
+
+		pageSidebarToggler.on('click', function() {
+			if (page_sidebar_open == 1) {
+				$('html').removeClass('pagesidebar_open');
+				pageSidebarToggler.removeClass('toggled');
+				page_sidebar_open = 0;
+			} else {
+				$('html').addClass('pagesidebar_open');
+				pageSidebarToggler.addClass('toggled');
+				page_sidebar_open = 1;
+			}
+		});
+
+		var pageSidebarClose = $('.page-sidebar .back');
+
+		pageSidebarClose.on('click', function() {
+			$('html').removeClass('pagesidebar_open');
+			pageSidebarToggler.removeClass('toggled');
+			page_sidebar_open = 0;
+		});
+		
+		toggle_page_sidebar = true;
+	}
+
+	if(!toggle_overlay_sidebar){
+		var overlaybutton = $('.sidenav-overlay-toggler');
+		if($('.wrapper').hasClass('is-show')){
+			overlay_sidebar_open = 1;
+			overlaybutton.addClass('toggled');
+			overlaybutton.html('<i class="icon-options-vertical"></i>');
+		}
+
+		overlaybutton.on('click', function() {
+			if (overlay_sidebar_open == 1) {
+				$('.wrapper').removeClass('is-show');
+				overlaybutton.removeClass('toggled');
+				overlaybutton.html('<i class="icon-menu"></i>');
+				overlay_sidebar_open = 0;
+			} else {
+				$('.wrapper').addClass('is-show');
+				overlaybutton.addClass('toggled');
+				overlaybutton.html('<i class="icon-options-vertical"></i>');
+				overlay_sidebar_open = 1;
+			}
+			$(window).resize();
+		});
+		minimize_sidebar = true;
+	}
+
+	$('.sidebar').hover(function() {
+		if ($('.wrapper').hasClass('sidebar_minimize')){
+			$('.wrapper').addClass('sidebar_minimize_hover');
+		}
+	}, function(){
+		if ($('.wrapper').hasClass('sidebar_minimize')){
+			$('.wrapper').removeClass('sidebar_minimize_hover');
+		}
+	});
+
+	// addClass if nav-item click and has subnav
+
+	$(".nav-item a").on('click', (function(){
+		if ( $(this).parent().find('.collapse').hasClass("show") ) {
+			$(this).parent().removeClass('submenu');
+		} else {
+			$(this).parent().addClass('submenu');
+		}
+	}));
+
+
+	//Chat Open
+	$('.messages-contact .user a').on('click', function(){
+		$('.tab-chat').addClass('show-chat')
+	});
+
+	$('.messages-wrapper .return').on('click', function(){
+		$('.tab-chat').removeClass('show-chat')
+	});
+
+	//select all
+	$('[data-select="checkbox"]').change(function(){
+		var target = $(this).attr('data-target');
+		$(target).prop('checked', $(this).prop("checked"));
+	})
+
+	//form-group-default active if input focus
+	$(".form-group-default .form-control").focus(function(){
+		$(this).parent().addClass("active");
+	}).blur(function(){
+		$(this).parent().removeClass("active");
+	})
+
+});
+
+// Input File Image
+
+function readURL(input) {
+	if (input.files && input.files[0]) {
+		var reader = new FileReader();
+
+		reader.onload = function (e) {
+			$(input).parent('.input-file-image').find('.img-upload-preview').attr('src', e.target.result);
+		}
+
+		reader.readAsDataURL(input.files[0]);
+	}
+}
+
+$('.input-file-image input[type="file"').change(function () {
+	readURL(this);
+});
+
+// Show Password
+
+function showPassword(button) {
+	var inputPassword = $(button).parent().find('input');
+	if (inputPassword.attr('type') === "password") {
+		inputPassword.attr('type', 'text');
+	} else {
+		inputPassword.attr('type','password');
+	}
+}
+
+$('.show-password').on('click', function(){
+	showPassword(this);
+})
+
+// Sign In & Sign Up
+var containerSignIn = $('.container-login'),
+containerSignUp = $('.container-signup'),
+showSignIn = true,
+showSignUp = false;
+
+function changeContainer(){
+	if(showSignIn == true){
+		containerSignIn.css('display', 'block')
+	} else {
+		containerSignIn.css('display', 'none')
+	}
+
+	if(showSignUp == true){
+		containerSignUp.css('display', 'block')
+	} else {
+		containerSignUp.css('display', 'none')
+	}
+}
+
+$('#show-signup').on('click', function(){ 
+	showSignUp = true;
+	showSignIn = false;
+	changeContainer();
+})
+
+$('#show-signin').on('click', function(){ 
+	showSignUp = false;
+	showSignIn = true;
+	changeContainer();
+})
+
+changeContainer();
+
+//Input with Floating Label
+
+$('.form-floating-label .form-control').keyup(function(){
+	if($(this).val() !== '') {
+		$(this).addClass('filled');
+	} else {
+		$(this).removeClass('filled');
+	}
+})
+
---
+++ examples/assets/js/demo.js
@@ -0,0 +1,288 @@
+"use strict";
+// Cicle Chart
+Circles.create({
+	id:           'task-complete',
+	radius:       50,
+	value:        80,
+	maxValue:     100,
+	width:        5,
+	text:         function(value){return value + '%';},
+	colors:       ['#36a3f7', '#fff'],
+	duration:     400,
+	wrpClass:     'circles-wrp',
+	textClass:    'circles-text',
+	styleWrapper: true,
+	styleText:    true
+})
+
+//Notify
+$.notify({
+	icon: 'flaticon-alarm-1',
+	title: 'Atlantis Lite',
+	message: 'Free Bootstrap 4 Admin Dashboard',
+},{
+	type: 'info',
+	placement: {
+		from: "bottom",
+		align: "right"
+	},
+	time: 1000,
+});
+
+// JQVmap
+$('#map-example').vectorMap(
+{
+	map: 'world_en',
+	backgroundColor: 'transparent',
+	borderColor: '#fff',
+	borderWidth: 2,
+	color: '#e4e4e4',
+	enableZoom: true,
+	hoverColor: '#35cd3a',
+	hoverOpacity: null,
+	normalizeFunction: 'linear',
+	scaleColors: ['#b6d6ff', '#005ace'],
+	selectedColor: '#35cd3a',
+	selectedRegions: ['ID', 'RU', 'US', 'AU', 'CN', 'BR'],
+	showTooltip: true,
+	onRegionClick: function(element, code, region)
+	{
+		return false;
+	}
+});
+
+//Chart
+
+var ctx = document.getElementById('statisticsChart').getContext('2d');
+
+var statisticsChart = new Chart(ctx, {
+	type: 'line',
+	data: {
+		labels: ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"],
+		datasets: [ {
+			label: "Subscribers",
+			borderColor: '#f3545d',
+			pointBackgroundColor: 'rgba(243, 84, 93, 0.6)',
+			pointRadius: 0,
+			backgroundColor: 'rgba(243, 84, 93, 0.4)',
+			legendColor: '#f3545d',
+			fill: true,
+			borderWidth: 2,
+			data: [154, 184, 175, 203, 210, 231, 240, 278, 252, 312, 320, 374]
+		}, {
+			label: "New Visitors",
+			borderColor: '#fdaf4b',
+			pointBackgroundColor: 'rgba(253, 175, 75, 0.6)',
+			pointRadius: 0,
+			backgroundColor: 'rgba(253, 175, 75, 0.4)',
+			legendColor: '#fdaf4b',
+			fill: true,
+			borderWidth: 2,
+			data: [256, 230, 245, 287, 240, 250, 230, 295, 331, 431, 456, 521]
+		}, {
+			label: "Active Users",
+			borderColor: '#177dff',
+			pointBackgroundColor: 'rgba(23, 125, 255, 0.6)',
+			pointRadius: 0,
+			backgroundColor: 'rgba(23, 125, 255, 0.4)',
+			legendColor: '#177dff',
+			fill: true,
+			borderWidth: 2,
+			data: [542, 480, 430, 550, 530, 453, 380, 434, 568, 610, 700, 900]
+		}]
+	},
+	options : {
+		responsive: true, 
+		maintainAspectRatio: false,
+		legend: {
+			display: false
+		},
+		tooltips: {
+			bodySpacing: 4,
+			mode:"nearest",
+			intersect: 0,
+			position:"nearest",
+			xPadding:10,
+			yPadding:10,
+			caretPadding:10
+		},
+		layout:{
+			padding:{left:5,right:5,top:15,bottom:15}
+		},
+		scales: {
+			yAxes: [{
+				ticks: {
+					fontStyle: "500",
+					beginAtZero: false,
+					maxTicksLimit: 5,
+					padding: 10
+				},
+				gridLines: {
+					drawTicks: false,
+					display: false
+				}
+			}],
+			xAxes: [{
+				gridLines: {
+					zeroLineColor: "transparent"
+				},
+				ticks: {
+					padding: 10,
+					fontStyle: "500"
+				}
+			}]
+		}, 
+		legendCallback: function(chart) { 
+			var text = []; 
+			text.push('<ul class="' + chart.id + '-legend html-legend">'); 
+			for (var i = 0; i < chart.data.datasets.length; i++) { 
+				text.push('<li><span style="background-color:' + chart.data.datasets[i].legendColor + '"></span>'); 
+				if (chart.data.datasets[i].label) { 
+					text.push(chart.data.datasets[i].label); 
+				} 
+				text.push('</li>'); 
+			} 
+			text.push('</ul>'); 
+			return text.join(''); 
+		}  
+	}
+});
+
+var myLegendContainer = document.getElementById("myChartLegend");
+
+// generate HTML legend
+myLegendContainer.innerHTML = statisticsChart.generateLegend();
+
+// bind onClick event to all LI-tags of the legend
+var legendItems = myLegendContainer.getElementsByTagName('li');
+for (var i = 0; i < legendItems.length; i += 1) {
+	legendItems[i].addEventListener("click", legendClickCallback, false);
+}
+
+var dailySalesChart = document.getElementById('dailySalesChart').getContext('2d');
+
+var myDailySalesChart = new Chart(dailySalesChart, {
+	type: 'line',
+	data: {
+		labels:["January",
+		"February",
+		"March",
+		"April",
+		"May",
+		"June",
+		"July",
+		"August",
+		"September"],
+		datasets:[ {
+			label: "Sales Analytics", fill: !0, backgroundColor: "rgba(255,255,255,0.2)", borderColor: "#fff", borderCapStyle: "butt", borderDash: [], borderDashOffset: 0, pointBorderColor: "#fff", pointBackgroundColor: "#fff", pointBorderWidth: 1, pointHoverRadius: 5, pointHoverBackgroundColor: "#fff", pointHoverBorderColor: "#fff", pointHoverBorderWidth: 1, pointRadius: 1, pointHitRadius: 5, data: [65, 59, 80, 81, 56, 55, 40, 35, 30]
+		}]
+	},
+	options : {
+		maintainAspectRatio:!1, legend: {
+			display: !1
+		}
+		, animation: {
+			easing: "easeInOutBack"
+		}
+		, scales: {
+			yAxes:[ {
+				display:!1, ticks: {
+					fontColor: "rgba(0,0,0,0.5)", fontStyle: "bold", beginAtZero: !0, maxTicksLimit: 10, padding: 0
+				}
+				, gridLines: {
+					drawTicks: !1, display: !1
+				}
+			}
+			], xAxes:[ {
+				display:!1, gridLines: {
+					zeroLineColor: "transparent"
+				}
+				, ticks: {
+					padding: -20, fontColor: "rgba(255,255,255,0.2)", fontStyle: "bold"
+				}
+			}
+			]
+		}
+	}
+});
+
+$("#activeUsersChart").sparkline([112,109,120,107,110,85,87,90,102,109,120,99,110,85,87,94], {
+	type: 'bar',
+	height: '100',
+	barWidth: 9,
+	barSpacing: 10,
+	barColor: 'rgba(255,255,255,.3)'
+});
+
+var topProductsChart = document.getElementById('topProductsChart').getContext('2d');
+
+var myTopProductsChart = new Chart(topProductsChart, {
+	type:"line",
+	data: {
+		labels:["January",
+		"February",
+		"March",
+		"April",
+		"May",
+		"June",
+		"July",
+		"August",
+		"September",
+		"October",
+		"January",
+		"February",
+		"March",
+		"April",
+		"May",
+		"June",
+		"July",
+		"August",
+		"September",
+		"October",
+		"January",
+		"February",
+		"March",
+		"April",
+		"May",
+		"June",
+		"July",
+		"August",
+		"September",
+		"October",
+		"January",
+		"February",
+		"March",
+		"April"],
+		datasets:[ {
+			label: "Sales Analytics", fill: !0, backgroundColor: "rgba(53, 205, 58, 0.2)", borderColor: "#35cd3a", borderCapStyle: "butt", borderDash: [], borderDashOffset: 0, pointBorderColor: "#35cd3a", pointBackgroundColor: "#35cd3a", pointBorderWidth: 1, pointHoverRadius: 5, pointHoverBackgroundColor: "#35cd3a", pointHoverBorderColor: "#35cd3a", pointHoverBorderWidth: 1, pointRadius: 1, pointHitRadius: 5, data: [20, 10, 18, 14, 32, 18, 15, 22, 8, 6, 17, 12, 17, 18, 14, 25, 18, 12, 19, 21, 16, 14, 24, 21, 13, 15, 27, 29, 21, 11, 14, 19, 21, 17]
+		}
+		]
+	},
+	options : {
+		maintainAspectRatio:!1, legend: {
+			display: !1
+		}
+		, animation: {
+			easing: "easeInOutBack"
+		}
+		, scales: {
+			yAxes:[ {
+				display:!1, ticks: {
+					fontColor: "rgba(0,0,0,0.5)", fontStyle: "bold", beginAtZero: !0, maxTicksLimit: 10, padding: 0
+				}
+				, gridLines: {
+					drawTicks: !1, display: !1
+				}
+			}
+			], xAxes:[ {
+				display:!1, gridLines: {
+					zeroLineColor: "transparent"
+				}
+				, ticks: {
+					padding: -20, fontColor: "rgba(255,255,255,0.2)", fontStyle: "bold"
+				}
+			}
+			]
+		}
+	}
+});
